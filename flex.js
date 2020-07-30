73bf44dbfcb5dd07e839fcc42c302e0ca1fe1612
Saba Akhtar <sabasaeedakhtar@gmail.com>
2020-07-30 10:25:20 +1000

    my task2

---
+++ flex.js
@@ -0,0 +1,19 @@
+ let button1=document.querySelector("#button1");
+ let button2=document.querySelector("#button2");
+//  changes text color
+ button1.onclick = function(){
+    document.getElementById("tx1").style.color = "red";
+    document.getElementById("tx1").style.fontFamily="comic sans";
+    document.getElementById("tx1").style.alignContent="justify";
+    
+    }
+    
+    
+
+button2.onclick = function(){
+   // alert("hello");
+$("p").toggle();
+
+}
+
+
